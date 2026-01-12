0e63a2a8b191b9fe05569ba860d0a478cf370294
walkzzz <745244185@qq.com>
2026-01-12 16:54:29 +0800

    Initial commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+  srcDir: "./docs", // 设置文档目录
+  // lastUpdated: true, // 开启最后更新时间
+  lastUpdated: {
+    text: "最后更新时间",
+    formatOptions: {
+      dateStyle: "medium",
+      // timeStyle: "medium",
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "HappyFree"
+  text: "人人都能学会编程"
+  tagline: 对，你能学会!
+  actions:
+    - theme: brand
+      text: 视频教程
+      link: https://space.bilibili.com/371312151
+    - theme: alt
+      text: 油猴开发
+      link: https://happyfe.com/fe/tampermonkey/learning-tampermonkey
+
+features:
+  - title: Python教程
+    details: 人人都能学，人人学的会
+  - title: 前端教程
+    details: 靠这个吃饭
+  - title: 杂七杂八
+    details: 每周往返于北京、天津的码农一枚
+---
